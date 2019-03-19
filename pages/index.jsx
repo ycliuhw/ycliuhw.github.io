29530c6240a467847fc0b4e8939c5578c34141f6
Kelvin Liu <kelvin.liu@canonical.com>
2019-03-20 00:11:25 +1100

    use next.js

---
+++ pages/index.jsx
@@ -0,0 +1,12 @@
+import Button from 'antd/lib/button';
+import css from "../style.css"
+
+
+function Home() {
+    return <div className="App">
+        Welcome to next.js!
+        <Button type="primary">Button</Button>
+    </div>
+}
+
+export default Home
